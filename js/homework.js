1f638f9e3e39be83179dfee79148c900921ee3cb
Antonio Servin <antonio.servin.m11@gmail.com>
2021-05-05 22:46:03 -0500

    Add instructions for homework exercises

--- js/homework.js
+++ js/homework.js
@@ -1,49 +1,56 @@
-// 1:
-// Pedir al usuario un numero entre 1 y 12
-// Imprimir en consola el numero de días que corresponden a ese mes y su nombre
-// p.ej.
-// --> El mes 5 (Mayo) tiene 31 días
+// Ejercicios 
+
+// Unicamente usar el ciclo While
+/** 
+ * 1: 
+ * Pedir al usuario usuario un numero entre 1 y 10
+ * Imprimir la tabla de multiplicar de ese número
+ * p.ej. 
+ * 3 x 1 = 3
+ * 3 x 2 = 6
+ */
 
 
 /**
  * 2:
- * Pedir al usuario que indique como esta su día
- * Opciones: Nublado, Soleado, Lluvioso, Nevado
- * Si escribe alguna de ellas, colocar un mensaje 
- * P.ej: soleado -> oh!, que buen clima para estar en la playa
- * P.ej: nevado -> oh!, que buen clima para estar en casa
- *  etc...
- * Sino
- * Pedir por prompt, Que describa que le gustaria hacer en su día
+ * Pedir al usuario una oracion
+ * Imprimir un string con todas las vocales
+ * Imprimir un string con todas las consonantes
+ * 
  */
 
 
 /**
  * 3:
- * Pedir el nombre de una persona
- * Verificar si el nombre termina con una vocal
- * imprimir que `Tu nombre termina con ${vocal}`
- * Sino
- * imprimir que `Tu nombre termina con ${consonante}`
- * -> Pedro
- * -> `Tu nombre termina con o`
+ * Pedir el usuario un numero entre 10 y 100
+ * Imprimir todos los numeros pares que existen entre 1 y ese numero
  */
+
+
+// Resolver 4 y 5 con For y While
 
 /**
  * 4:
- * Pedir el nombre de una persona
- * Imprimir el nombre de la persona al revés y en mayúsculas
- * P.ej: juan
- * -> NAUJ
+ * Pedir el usuario una oracion
+ * Imprimir en consola si una oración es un palindromo (puede contener espacios)
+ * p.ej. anita lava la tina 
+ * -> La oración "anita lava la tina" es un palindromo
  */
-
 
 /**
- * Pedir una palabra por promt
- * Pedir un número entre 1 y 10
- * Duplicar esa cantidad de veces esa palabra e imprimir en consola
- * P.ej: 
- * "Kodemia"
- * 4
- * -> "Kodemia Kodemia Kodemia Kodemia"
- */
+ * 5:
+ * pedir el usuario un numero entre 1 y 10
+ * Imprimir en consola el siguiente patrón
+ * donde la cantidad mas grande de * es el Numero del usuario
+ * P.ej. el usuario introduce 5 
+ * Resultado esperado
+ *
+ **
+ ***
+ ****
+ *****
+ ****
+ ***
+ **
+ *
+*/
