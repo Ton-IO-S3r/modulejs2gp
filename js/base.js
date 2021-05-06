3d0ff6b32df6a588715346b965d597e3fe0a5295
Antonio Servin <antonio.servin.m11@gmail.com>
2021-05-05 22:44:47 -0500

    Add solutions for the exercises from class using WHILE

--- js/base.js
+++ js/base.js
@@ -1,18 +1,12 @@
-const userName = "Jorge";
-const counter = 5;
-/* Ciclo FOR
-    for(inicio; fin; inc/dec){
-        sentencias
-    }
-*/
-/* INCREMENTO */
-/* for (let i = 0; i < counter; i++) {
-    console.log(userName);
-    
-} */
+            /* Ciclo FOR
+                for(inicio; fin; inc/dec){
+                    sentencias
+                }
+            */
 
-/*Ejercicios+/
-/**
+
+/*Ejercicios CLASE+/
+/**1
  * Pedir al usuario usuario un numero entre 1 y 10
  * Imprimir la tabla de multiplicar de ese número
  * p.ej. 
@@ -26,9 +20,7 @@ for (let i = 1; i <= 10; i++) {
 }
 
 
-
-
-/**
+/**2
  * Pedir al usuario una oracion
  * Imprimir un string con todas las vocales
  * Imprimir un string con todas las consonantes
@@ -38,19 +30,32 @@ for (let i = 1; i <= 10; i++) {
  let vocales='', consonantes='';
 
  for (let i = 0; i < sentence.length; i++) {
-     if(sentence[i] === 'a' || sentence[i] === 'e' || sentence[i] === 'i' || sentence[i] === 'o' || sentence[i] === 'u'){
+     if(
+         sentence[i] === 'a' || 
+         sentence[i] === 'e' || 
+         sentence[i] === 'i' || 
+         sentence[i] === 'o' || 
+         sentence[i] === 'u' ||
+         sentence[i] === 'á' ||
+         sentence[i] === 'é' ||
+         sentence[i] === 'í' ||
+         sentence[i] === 'ó' ||
+         sentence[i] === 'ú' 
+         ){
         vocales += sentence[i];
-     } else if (sentence[i] != ' ' && sentence[i] != ',' && sentence[i] != '.'){
-         consonantes += sentence[i];
+    } else if (
+        sentence[i] != ' ' && 
+        sentence[i] != ',' && 
+        sentence[i] != '.'
+    ){
+        consonantes += sentence[i];
      }
  }
  console.log(vocales)
  console.log(consonantes)
 
 
-
-
-/**
+/**3
  * Pedir el usuario un numero entre 10 y 100
  * Imprimir todos los numeros pares que existen entre 1 y ese numero
  */
@@ -60,3 +65,79 @@ for (let i = 1; i <= 10; i++) {
         console.log(i)    
      }
  }
+
+
+            /* While */
+            /* 
+                    while(condition){
+                        sentencias
+                    } 
+            */
+
+/**1
+ * Pedir al usuario usuario un numero entre 1 y 10
+ * Imprimir la tabla de multiplicar de ese número
+ * p.ej. 
+ * 3 x 1 = 3
+ * 3 x 2 = 6
+ */
+ const number = parseInt(window.prompt('Ingresa un numero entre 1 y 10'));
+ let i=1;
+ while (i<=10) {
+    console.log(`${number} x ${i} = ${number*i}`);
+    i++;
+ }
+ 
+
+
+/**2
+ * Pedir al usuario una oracion
+ * Imprimir un string con todas las vocales
+ * Imprimir un string con todas las consonantes
+ * 
+ */
+const sentence = window.prompt('Ingresa una oracion');
+let vocales='', consonantes='';
+let i=0;
+console.log(sentence)
+while (i < sentence.length) {
+    if(
+        sentence[i] != ' ' && 
+        sentence[i] != ',' && 
+        sentence[i] != '.'
+    ){
+        if(
+            sentence[i] === 'a' || 
+            sentence[i] === 'e' || 
+            sentence[i] === 'i' || 
+            sentence[i] === 'o' || 
+            sentence[i] === 'u' ||
+            sentence[i] === 'á' ||
+            sentence[i] === 'é' ||
+            sentence[i] === 'í' ||
+            sentence[i] === 'ó' ||
+            sentence[i] === 'ú' 
+        ){
+           vocales += sentence[i];
+        }else{
+            consonantes += sentence[i];
+        }
+    }
+    i++;   
+}
+console.log(vocales)
+console.log(consonantes)
+       
+/**3
+ * Pedir el usuario un numero entre 10 y 100
+ * Imprimir todos los numeros pares que existen entre 1 y ese numero
+ */
+ const number = parseInt(window.prompt('Ingresa un numero entre 1 y 100'));
+ let i=1;
+ while (i<= number) {
+    if (i%2 == 0 ) {
+        console.log(i)    
+    }
+    i++
+ }
+ 
