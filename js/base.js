0352c99e98861bc3cc094d37e8e26bb101f550a4
Antonio Servin <antonio.servin.m11@gmail.com>
2021-05-06 20:01:41 -0500

    Add third exercise from class 4

--- js/base.js
+++ js/base.js
@@ -3,14 +3,14 @@
  * Escribir un programa de arroje la suma de los multiplos de 3, 5 o 7 que hay entre 1 y 100
  * 
  */
-let sumatoria = 0;
+/* let sumatoria = 0;
 for (let i = 3; i <= 100; i++) {
     if (i%3 == 0 || i%5==0 || i%7==0) {
         sumatoria +=i
         console.log(i, sumatoria)
     }
 }
-console.log(`La suma de los multiplos de 3, 5 o 7 entre 1 y 100 es : ${sumatoria}`);
+console.log(`La suma de los multiplos de 3, 5 o 7 entre 1 y 100 es : ${sumatoria}`); */
 
 
  /**
@@ -20,12 +20,32 @@ console.log(`La suma de los multiplos de 3, 5 o 7 entre 1 y 100 es : ${sumatoria
   * si el numero es negativo, salir del ciclo
   * 
   */
- let numero=0;
+ /* let numero=0;
  while (numero>=0) {
     numero = parseInt(window.prompt('Escribe un numero:'))
     if (numero >=0) {
         console.log(numero)
     }
- }
+ } */
  
- 
+ /**
+ * Dado los siguientes 2 strings
+ * 'smtp'
+ * 'aeiou'
+ * Imprimir en consola las siguientes combinaciones
+ * -> sa, se, si, so , su 
+ * -> ma, me, mi, mo , mu 
+ * -> ta, te, ti, to , tu 
+ * -> pa, pe, pi, po , pu
+ */
+const consonants = 'smtp';
+const vowels = 'aeiou';
+let combinations='';
+for (let i = 0; i < consonants.length; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+        combinations += consonants[i].concat(vowels[j]).concat(', '); 
+        
+    }
+    console.log(combinations.slice(0,combinations.length-2))
+    combinations='';
+}
