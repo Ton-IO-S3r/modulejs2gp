b16b7a962cade0bf08b651ab54010e14bab05fb4
Antonio Servin <antonio.servin.m11@gmail.com>
2021-05-06 19:39:28 -0500

    Create branch for class four, edit the structure and add first class exercises

--- js/base.js
+++ js/base.js
@@ -1,75 +1,31 @@
-            /* Ciclo FOR
-                for(inicio; fin; inc/dec){
-                    sentencias
-                }
-            */
-
-
-/*Ejercicios CLASE+/
-/**1
- * Pedir al usuario usuario un numero entre 1 y 10
- * Imprimir la tabla de multiplicar de ese número
- * p.ej. 
- * 3 x 1 = 3
- * 3 x 2 = 6
- */
-
-const number = parseInt(window.prompt('Ingresa un numero entre 1 y 10'));
-for (let i = 1; i <= 10; i++) {
-    console.log(`${number} x ${i} = ${number*i}`)
-}
-
-
-/**2
- * Pedir al usuario una oracion
- * Imprimir un string con todas las vocales
- * Imprimir un string con todas las consonantes
+/**
+ * For
+ * Escribir un programa de arroje la suma de los multiplos de 3, 5 o 7 que hay entre 1 y 100
  * 
  */
- const sentence = window.prompt('Ingresa una oracion');
- let vocales='', consonantes='';
+let sumatoria = 0;
+for (let i = 3; i <= 100; i++) {
+    if (i%3 == 0 || i%5==0 || i%7==0) {
+        sumatoria +=i
+        console.log(i, sumatoria)
+    }
+}
+console.log(`La suma de los multiplos de 3, 5 o 7 entre 1 y 100 es : ${sumatoria}`);
 
- for (let i = 0; i < sentence.length; i++) {
-     if(
-         sentence[i] === 'a' || 
-         sentence[i] === 'e' || 
-         sentence[i] === 'i' || 
-         sentence[i] === 'o' || 
-         sentence[i] === 'u' ||
-         sentence[i] === 'á' ||
-         sentence[i] === 'é' ||
-         sentence[i] === 'í' ||
-         sentence[i] === 'ó' ||
-         sentence[i] === 'ú' 
-         ){
-        vocales += sentence[i];
-    } else if (
-        sentence[i] != ' ' && 
-        sentence[i] != ',' && 
-        sentence[i] != '.'
-    ){
-        consonantes += sentence[i];
-     }
+
+ /**
+  * While
+  * Escribir un programa que le pida al usuario un numero
+  * que imprima en consola el numero dado si es positivo
+  * si el numero es negativo, salir del ciclo
+  * 
+  */
+ let numero=0;
+ while (numero>=0) {
+    numero = parseInt(window.prompt('Escribe un numero:'))
+    if (numero >=0) {
+        console.log(numero)
+    }
  }
- console.log(vocales)
- console.log(consonantes)
-
-
-/**3
- * Pedir el usuario un numero entre 10 y 100
- * Imprimir todos los numeros pares que existen entre 1 y ese numero
- */
- const number2 = parseInt(window.prompt('Ingresa un numero entre 1 y 100'));
- for (let i = 1; i <= number2; i++) {
-     if (i%2 == 0 ) {
-        console.log(i)    
-     }
- }
-
-
-            /* While */
-            /* 
-                    while(condition){
-                        sentencias
-                    } 
-            */
+ 
+ 
