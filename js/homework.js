1562a9324b6d64d6b3fd3e4ee3bf0627992a6087
Antonio Servin <antonio.servin.m11@gmail.com>
2021-05-13 19:37:38 -0500

    Add homework of class seven

--- js/homework.js
+++ js/homework.js
@@ -1,35 +1,34 @@
-// Metodos de array
+/**
+ * 1:
+ * Estudiar los metodos
+ * .map()
+ * .reduce()
+ */
 
-/*
-Escribir una funcion que tome como parametro un array de numeros e imprima en
-consola el array al revés
-arrayReverse([1,2,3,4]). 
--> [4,3,2,1]
-*/
-const reverseArray = (myArray) => myArray.reverse();
 
-/*
-Escribir una funcion que tome como parametro 2 arrays, y devuelva un numero 
-con la suma total de esos 2 arrays.
-arrayReduce([1,2,3,4],[1,2]). 
--> 13
+/**
+ * Ejericio 1:
+ * Realizar una funcion que dado un array de strings como parametro
+ * retorne un nuevo array con todos los strings que tienen como maximo 5 caracteres
+ */
+
+/*  const fiveCharactersOnly = arr => arr.filter(element => element.length <= 5)
+
+// test
+let responseFiveCharacters = fiveCharactersOnly(["dog", "wolf", "family", "eaten", "camping"]) */
+// [ "dog", "wolf", "eaten"]
+
+
+/**
+* Ejericio 2:
+* Realizar una funcion que dado un array de nombres de koders como parametro
+* Retorne un nuevo array con todos koders que tienen mas de 1 nombre
+* 
 */
-const sumArrays = (array1,array2) => {
-  var result=0;
-  var arraySum = array1.concat(array2)
-  arraySum.forEach(element => result+=element);
-  return result
-}
-/*
-Escribir una función que tome como parametro 2 arrays, y devuelva un array 
-con los numeros repetidos en los 2 arrays
-arrayReduce([1,2,3,4],[1,2]). 
--> [1,2]
-*/
-const filterRepeated = (array1,array2) => {
-  var resultArray=[];
-  array2.forEach(element =>{
-    if (array1.includes(element)) resultArray.push(element)
-  })
-  return resultArray
-}
+
+
+const twoNamesKoders = arr => arr.filter(koderName => koderName.split(" ").length > 1)
+
+// test
+let responseTwoNamesKoders = twoNamesKoders(["Antonio", "Victor Yair", "Luis Carlos", "Victor", "José Omar"])
+// ["Victor Yair", "Luis Carlos", "José Omar"]
