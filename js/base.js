cd41b112a4694f9b2fc22f8295525801eb98c750
Antonio Servin <antonio.servin.m11@gmail.com>
2021-05-23 00:42:33 -0500

    Add exercise using window.location pathname to show koder card

--- js/base.js
+++ js/base.js
@@ -4,7 +4,7 @@ let koders =[
   {
     id: 1,
     name: 'Jorge',
-    bio: 'lorem lorem lorem  lorem',
+    bio: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, laborum tempore maxime aliquam nemo cumque',
     git: '/dered-dev',
     imgKoder:'https://avatars.githubusercontent.com/u/2187296?v=4'
   },
@@ -36,37 +36,44 @@ let koders =[
     git: '/yairavalos',
     imgKoder:'https://avatars.githubusercontent.com/u/82552876?v=4'
   },
-]
-if (window.location.pathname='clase.html') {
+];
+if (window.location.pathname === '/pages/clase.html') {
   const domList = document.querySelector('.domList')
   let acc=''
   koders.forEach(koder=>{
-    let {id, name, bio, git, imgKoder} = koder
+    let {id, name, git} = koder
     acc += `
-        <li>${name}</li>
-
+        <li class="list-group-item d-flex justify-content-between">
+         <h3>${name}</h3>
+         <div>
+          <a class="btn btn-info profile mx-4" href='/pages/koder.html?idkoder=${id}'>Profile</a>
+          <a class="btn btn-secondary mx-4 github" href="https://github.com${git}" target='_blank'>Github</a>
+         </div> 
+        </li>
     `
     
   })
   domList.innerHTML=acc
 }
+if (window.location.pathname === '/pages/koder.html') {
+  let search = window.location.search;
+  let idkoder = parseInt(search.slice(search.indexOf('=')+1))
+  console.log(idkoder)
 
+  let koderObject = koders.filter(koder => koder.id == idkoder)[0]
 
-let search = window.location.search;
-let idkoder = parseInt(search.slice(search.indexOf('=')+1))
-console.log(idkoder)
+  console.log(koderObject)
 
-let koderObject = koders.filter(koder => koder.id == idkoder)[0]
-
-console.log(koderObject)
-
-const printKoder = (objKoder)=>{
-  let {id, name, bio, git, imgKoder}=objKoder
-  document.querySelector('.card-img-top').setAttribute('src',`${imgKoder}`)
-  document.querySelector('.card-title').innerText = `${id}: ${name}`
-  document.querySelector('.card-text').innerText = bio
-  document.querySelector('.btn-primary').innerText = git
-  document.querySelector('.btn-primary').setAttribute('href',`https://github.com${git}`)
-  document.querySelector('.btn-primary').setAttribute('target',`_blank`)
+  const printKoder = (objKoder)=>{
+    let {id, name, bio, git, imgKoder}=objKoder
+    document.querySelector('.card-img-top').setAttribute('src',`${imgKoder}`)
+    document.querySelector('.card-title').innerText = `Koder ${id}: ${name}`
+    document.querySelector('.card-text').innerText = bio
+    document.querySelector('.btn-primary').innerText = git
+    document.querySelector('.btn-primary').setAttribute('href',`https://github.com${git}`)
+    document.querySelector('.btn-primary').setAttribute('target',`_blank`)
+  }
+  printKoder(koderObject)
 }
-printKoder(koderObject)
+
+
